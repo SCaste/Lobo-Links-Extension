9035b5ca10b8016ef44ac58c1666fbfe8a69f217
SCaste <SCaste@users.noreply.github.com>
2017-07-19 17:23:00 -0600

    Add files via upload

---
+++ Lobo Links/finals.js
@@ -0,0 +1,4 @@
+function finalsButton() {
+	window.open("http://schedule.unm.edu/final-exams/final_exam/spring-2017-final-exam.pdf");
+}
+document.getElementById("open_finals").onclick = finalsButton    
