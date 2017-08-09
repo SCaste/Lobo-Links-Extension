2aff22d0471f55502ee3ac06c251c6d3f0a3c744
SCaste <SCaste@users.noreply.github.com>
2017-08-08 18:37:47 -0600

    Add files via upload

--- Lobo Links/gymStatus.js
+++ Lobo Links/gymStatus.js
@@ -4,7 +4,7 @@
  */
 
 window.addEventListener("load",function gymStatus() {
-	var gymState = ["pics\\gymOpen.png",
+	var gym_state = ["pics\\gymOpen.png",
 	       		"pics\\gymClosed.png"];	
 
 	var now = new Date(); 
@@ -14,17 +14,18 @@ window.addEventListener("load",function gymStatus() {
 	Source:http://recsvcs.unm.edu/hours/Hours.html 
      */
         //Weekdays	
-	if (now.getDay >= 1 && now.getDay <= 5) {
-		if (how.getHours() > 6 && now.getHours() < 21 ){
-			document.getElementById("gym_status").src = gymState[0];
+	if (now.getDay() >= 1 && now.getDay() <= 5) {
+		console.log(now.getDay() + " " + now.getHours());
+		if (now.getHours() > 6 && now.getHours() < 21 ){
+			document.getElementById("gym_status").src = gym_state[0];
 		}	
 	}
 	//Sunday and Saturday
-	if (now.getDay == 0 || now.getDay == 6) {    
+	else if (now.getDay() == 0 || now.getDay() == 6) {    
 	       if (now.getHours() >= 12 && now.getHours() <= 19) {
-		       document.getElementById("gym_status").src = gymState[0];
+		       document.getElementById("gym_status").src = gym_state[0];
 	       }
 	}
-	else document.getElementById("gym_status").src = gymState[1];
+	else   document.getElementById("gym_status").src = gym_state[1]; 
 });
 		
